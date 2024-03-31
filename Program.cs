0e49a81577106eaf7d8bb3a8495d5f89ad1db466
Navatusein <boghdan.kutsulima@gmail.com>
2024-03-31 16:39:41 +0300

    Small fixes

--- Program.cs
+++ Program.cs
@@ -124,7 +124,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(option =>
+    {
+        option.DocumentTitle = "User Profile API";
+    });
 }
 
 // Add Exception Handling Middleware
